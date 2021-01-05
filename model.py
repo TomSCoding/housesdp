d2bcf0970dcb4a2ebedd716d6ffdcef29d350b5e
tomscoding <stankiewicztommy82@gmail.com>
2021-01-05 00:09:00 +0000

    initial commit

---
+++ model.py
@@ -0,0 +1,11 @@
+import numpy as np
+from random import randint
+from sklearn.utils import shuffle
+from sklearn.preprocessing import MinMaxScaler
+
+#Train Data
+train_labels = []
+train_samples = []
+
+
+
